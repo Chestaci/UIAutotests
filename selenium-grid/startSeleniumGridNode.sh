465db991aa3b7f83fef31305bf0def3b2e9e81d8
Anastasia <chestaci@gmail.com>
2023-05-03 17:13:27 +0300

    U7

--- selenium-grid/startSeleniumGridNode.sh
+++ selenium-grid/startSeleniumGridNode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the node
-java -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-4.9.0.jar node --config nodeconfig.toml
+java -jar selenium-server-4.9.0.jar node --config nodeconfig.toml
